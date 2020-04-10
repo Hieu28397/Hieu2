8e605601bf8be4e881051aaf3168df5b359acc8e
hieu28397 <59332419+Hieu28397@users.noreply.github.com>
2020-04-10 16:25:53 +0700

    first commit

---
+++ OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            var model = new ProductDao().ListAll();
+            return View(model);
+        }
+    
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult Edit(long id)
+        //{
+        //    var dao = new ProductDao();
+        //    var product = dao.GetByID(id);
+
+        //    SetViewBag(product.CategoryID);
+
+        //    return View(product);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Edit(Product model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+
+        //    }
+        //    SetViewBag(model.CategoryID);
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult Create(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            SetViewBag();
+            return View();
+        }
+
+        public void SetViewBag(long? selectedID = null)
+        {
+            var dao = new ProductCategoryDao();
+            ViewBag.CategoryID = new SelectList(dao.ListAll(), "ID", "Name", selectedID);
+        }
+    }
+}
